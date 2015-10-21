d8c41c821671f99793e80e5695b1a6de96e97a1a
Paul Giblock <p@pgiblock.net>
2015-10-21 17:37:26 -0400

    Almost there. Need to add CLI options
    ... and tweak a few design choices and refactor some code

---
+++ README.md
@@ -0,0 +1,8 @@
+# Installing
+
+You don't have to install massextract, but you will need some dependencies:
+
+  - pip install lockfile
+  - pip install patool
+
+# Running  
--- massextract.py
+++ massextract.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 import argparse
+import hashlib
+import json
 import lockfile
 import os, os.path
 import patoolib
 import shutil
+import stat
+import sys
 
-ARCHIVE_FILES = ['.7z', '.bz2', '.gz', '.rar', '.tar', '.xz', '.zip']
-COPY_FILES    = ['.avi', '.flac', '.mkv', '.mp3', '.mp4', '.ogg']
-
-in_root_dir  = '/Users/pgiblock'
-out_root_dir = '/Users/pgiblock/out'
+ARCHIVE_FILES   = ['.7z', '.bz2', '.gz', '.rar', '.tar', '.xz', '.zip']
+COPY_FILES      = ['.avi', '.flac', '.mkv', '.mp3', '.mp4', '.ogg']
+INDEX_NAME      = '.massextract'
+LOCKFILE_NAME   = '/tmp/massextract'
+HASH_BLOCK_SIZE = 1 << 16 # 64k
 
 # Patoolib has a bug where invoking 7z waits for the user to confirm
 # file overwrite. Apply this and any other hotfixes here
+# TODO: Remove this whole function once patoolib isn't dumb
 def hotfix_patoolib():
     import patoolib.programs.p7zip as p7zip
     import patoolib.programs.unrar as unrar
@@ -57,32 +62,122 @@ def classify_file(fname):
     else:
         return None
 
+# Return filename of index for directory
+def index_for_dir(directory):
+    return os.path.join(directory, INDEX_NAME)
+
+def load_index(directory):
+    fname = index_for_dir(directory)
+    try:
+        f = open(fname, 'r')
+        # TODO: Handle error (don't want a partial index) Rebuild?
+        try:
+            return json.load(f)
+        except ValueError as e:
+            # Problem interpreting JSON, check if empty file
+            st = os.stat(fname)
+            if stat.S_ISREG(st.st_mode) and st.st_size == 0:
+                print 'WARN: Index %s was empty, ignoring.' % fname
+                return {}
+            # Bubble any other error back to the top
+
+    except IOError as e:
+        if os.errno.ENOENT:
+            # Directory hasn't been indexed. Start fresh
+            return {}
+        # Bubble any other error back to the top
+
+def save_index(directory, idx):
+    # Don't pollute with empty files
+    if len(idx) > 0:
+        fname = index_for_dir(directory)
+        with open(fname, 'w') as f:
+            return json.dump(idx, f)
+
+# Hash a file. Do it in pieces to reduce memory footprint
+def hash_file(file_path):
+    sha = hashlib.sha512()
+    with open(file_path, 'r') as f:
+        while True:
+            data = f.read(HASH_BLOCK_SIZE)
+            if not data:
+                break
+            sha.update(data)
+    return sha.hexdigest()    
+
 # This is the main interface
-@lockfile.locked('/tmp/massextract')
-def massextract():
+@lockfile.locked(LOCKFILE_NAME)
+def massextract(in_root_dir, out_root_dir, count_threshold):
     for dir_name, dirs, files in os.walk(in_root_dir):
         rel_dir = os.path.relpath(dir_name, in_root_dir)
         out_dir = os.path.normpath(os.path.join(out_root_dir, rel_dir))
+
+	# open index file for rel_dir, used to check file completeness
+	idx = load_index(dir_name)
+        print 'LOL', repr(idx)
 
         # Don't bother checking the finger print of unknown extensions
         #for f, t in filter(lambda x: bool, map(classify_file, files)):
         for f, t in filter(None, map(classify_file, files)):
             file_path = os.path.join(dir_name, f)
+            f = unicode(f, sys.getfilesystemencoding())
+            print 'LOL', repr(file_path)
 
-            # Got a matching extension, preemptively create output dir
             try:
-                os.makedirs(out_dir)
-            except OSError as e:
-                # Directory already exists is OK. Bubble anything else back up
-                if e.errno == os.errno.EEXIST:
-                    pass
+                state       = idx[f] # !! FIXME
+                # Old sum: to check if file changed, or if we are ready to copy
+                old_sum     = state['shasum']
+                # The current check count (one more than last time)
+                cnt         = state['cnt'] + 1
+                # Has the file been processed?
+                processed   = state['processed']
+            except KeyError:
+                # File not indexed: no sum
+                old_sum = ''
+                # First iteration
+                old_cnt = 1
+                # Never been processed
+                processed = False
 
-            # Now perform the appropriate copy/extract operation, t, on the file
-            try:
-                t(file_path, out_dir)
-            except Exception as e:
-                print 'WARN: could not process %s: %s' % (file_path, e.message)
+            if not processed:
+                # File is not known to be stable: calculate new hash
+                new_sum = hash_file(file_path)
+                if new_sum == old_sum:
+                    # Same hash, increment the count
+                    new_cnt = old_cnt + 1
+                else:
+                    # Different hash, reset the count
+                    new_cnt = 1
+
+                if new_cnt >= count_threshold:
+                    # File hasn't been processed and is ready: process!
+
+                    # Prepare output directory
+                    try:
+                        os.makedirs(out_dir)
+                    except OSError as e:
+                        # Directory already exists is OK. Bubble anything else back up
+                        if e.errno == os.errno.EEXIST:
+                            pass
+
+                    # Now perform the appropriate copy/extract operation, t, on the file
+                    try:
+                        t(file_path, out_dir)
+                        processed = True
+                    except Exception as e:
+                        print 'WARN: could not process %s: %s' % (file_path, e.message)
+
+                # Update the index based on what we just did
+                idx[f] = {'shasum': new_sum, 'cnt': new_cnt, 'processed': processed}
+        
+        # Done with files in directory: rewrite the index
+        save_index(dir_name, idx)
+
+########
 
 if __name__ == '__main__':
+    in_root_dir     = '/Users/pgiblock/src/massextract/test-in'
+    out_root_dir    = '/Users/pgiblock/src/massextract/test-out'
+    count_threshold = 0
     hotfix_patoolib()
-    massextract()
+    massextract(in_root_dir, out_root_dir, count_threshold)
